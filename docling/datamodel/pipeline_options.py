e7426fb5b3c6a7b00f5e481f670eef5e5d7d3299
Yaroslava Tetyukhina <ya.tetyukhina@cism-ms.ru>
2025-04-13 18:18:32 +0300

    add ru lang pipeline options

--- docling/datamodel/pipeline_options.py
+++ docling/datamodel/pipeline_options.py
@@ -155,7 +155,7 @@ class EasyOcrOptions(OcrOptions):
     """Options for the EasyOCR engine."""
 
     kind: ClassVar[Literal["easyocr"]] = "easyocr"
-    lang: List[str] = ["fr", "de", "es", "en", 'ru']
+    lang: List[str] = ["fr", "de", "es", "en", 'ru', "rs_cyrillic"]
 
     use_gpu: Optional[bool] = None
 
